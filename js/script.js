91ab4b4263187d22d0bc3779462701c5cd7c2309
crlimon11 <crlimon24@gmail.com>
2023-07-31 10:19:07 +0600

    complet

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $('.bannar_main_slider').slick({
     arrows:false,
-    dots:true
+    dots:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
+$('.card_main_slider').slick({
+    arrows:true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-right"></i>'
 });
