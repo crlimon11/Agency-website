6af800260e802bcac802ceadab1b5356739c2f07
crlimon11 <crlimon24@gmail.com>
2023-08-03 18:24:48 +0600

    reply

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $('.bannar_main_slider').slick({
     dots:true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 1000,
 });
 $('.card_main_slider').slick({
@@ -11,8 +11,10 @@ $('.card_main_slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
+    // autoplay: true,
+    // autoplaySpeed: 1000,
     prevArrow: '<i class="fa-solid fa-angle-left"></i>',
-    nextArrow: '<i class="fa-solid fa-angle-right"></i>'
+    nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+    centerMode: true,
+    centerPadding: '0',
 });
