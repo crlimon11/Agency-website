5cdb2295695d9f1c4f41cd5b754894f0108dad10
crlimon11 <crlimon24@gmail.com>
2023-08-08 14:46:36 +0600

    xtra resposive

--- js/script.js
+++ js/script.js
@@ -17,4 +17,23 @@ $('.card_main_slider').slick({
     nextArrow: '<i class="fa-solid fa-angle-right"></i>',
     centerMode: true,
     centerPadding: '0',
+    responsive: [
+      {
+        breakpoint: 575.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
 });
